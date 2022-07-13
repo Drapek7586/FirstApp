11e28ac3d11d1c3e40d50f20082b6730261a502e
Drapek7586 <109133840+Drapek7586@users.noreply.github.com>
2022-07-13 12:40:36 +0200

    Add files via upload

---
+++ RandomDataStorage/RandomDataManager.cs
@@ -0,0 +1,21 @@
+﻿using RealTimeAPIReader.Models;
+namespace RealTimeAPIReader.RandomDataStorage
+{
+    public class DataManager
+    {
+            public static List<ChartModel> GetData()
+            {
+                var r = new Random();
+                return new List<ChartModel>()
+            {
+                //Random data for simulationg charts
+
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Losowa dana1", BackgroundColor = "#5491DA" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Losowa dana 2", BackgroundColor = "#E74C3C" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Losowa dana 3", BackgroundColor = "#82E0AA" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Losowa dana 4", BackgroundColor = "#E5E7E9" }
+            };
+            }
+        }
+    }
+
